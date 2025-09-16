da731117861077f993ade2c8202c6b3b8cfc0c2e
DDurdey <daniel.durdey@edu.sait.ca>
2025-09-16 16:57:20 -0600

    Created week2/page, created week2/studentinfo, removed all app/page code and connected week2

---
+++ app/week2/student-info.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function StudentInfo() {
+    return (
+        <div>
+            <p>Daniel Durdey</p>
+            <p>
+                My GitHub repository:{' '}
+                <a href="https://github.com/DDurdey/cprg306-assignments" target="_blank" rel="noopener noreferrer">
+                    https://github.com/DDurdey/cprg306-assignments
+                </a>
+            </p>
+        </div>
+    );
+}
+
+export default StudentInfo;

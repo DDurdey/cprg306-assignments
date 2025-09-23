5c6ddb1473758e15bd40db16da176a60fffaa890
DDurdey <daniel.durdey@edu.sait.ca>
2025-09-23 16:58:10 -0600

    changed width of box

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,7 +2,7 @@
 
 export default function Item({ name, quantity, category }) {
     return (
-        <div className="border-4 border-black rounded-lg p-4 mb-4 w-128 bg-gradient-to-b from-grey-400 to-blue-700 shadow-md">
+        <div className=" border-4 border-black rounded-lg p-4 mb-4 w-full bg-gradient-to-b from-grey-400 to-blue-700 shadow-md">
             <p className="text-lg font-semibold text-center">{name}</p>
             <p className="text-center">Quantity: {quantity}</p>
             <p className="text-center">Category: {category}</p>
